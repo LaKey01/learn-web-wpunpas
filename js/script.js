ab236b0a396ee7095631629b40704c8a2a0ee391
1nLakey <putratririzki77@gmail.com>
2025-07-03 01:06:26 +0800

    products icons checkpoint

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const navbarNav = document.querySelector(".navbar-nav");
 const hamburger = document.querySelector("#hamburger-menu");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search");
+const originalPlaceholder = searchBox.placeholder;
 const docs = document;
 
 // Toggle sidebar visibility
@@ -7,12 +11,6 @@ hamburger.onclick = (event) => {
   navbarNav.classList.toggle("active");
   event.preventDefault();
 };
-
-// Toggle search form visibility
-const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
-const originalPlaceholder = searchBox.placeholder;
-const searchButton = document.querySelector("#search");
 
 searchButton.addEventListener("click", function (event) {
   searchForm.classList.toggle("active");
@@ -22,14 +20,38 @@ searchButton.addEventListener("click", function (event) {
   } else {
     searchBox.placeholder = "";
   }
+
+  // if (
+  //   !searchButton.contains(event.target) &&
+  //   !searchForm.contains(event.target)
+  // ) {
+  //   searchForm.classList.remove("active");
+  // }
   event.preventDefault();
 });
 
 searchBox.placeholder = originalPlaceholder;
 
+// // docs.addEventListener("click", function (event) {
+// //   if (
+// //     !searchButton.contains(event.target) &&
+// //     !searchForm.contains(event.target)
+// //   ) {
+// //     searchForm.classList.remove("active");
+// //     searchBox.placeholder = "";
+// //   }
+// });
+
 // Close sidebar when clicking outside of it
 docs.addEventListener("click", function (event) {
   if (!hamburger.contains(event.target) && !navbarNav.contains(event.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (
+    !searchButton.contains(event.target) &&
+    !searchForm.contains(event.target)
+  ) {
+    searchForm.classList.remove("active");
+    searchBox.placeholder = "";
   }
 });
